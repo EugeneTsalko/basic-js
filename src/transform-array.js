418f054ba2ab75c2b9ec96368f9c9c096adcfd3d
EugeneTsalko <tefworkmail@gmail.com>
2022-05-02 14:32:49 +0300

    fixed transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(arr) {
+ function transform(arr) {
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
   if(!Array.isArray(arr)) {
@@ -24,28 +24,37 @@ function transform(arr) {
   
   let array = arr.slice(); // копия входного массива, т.к. его изменять нельзя
   for (let i = 0; i < array.length; i++) {
+    console.log(i)
     if (array[i] === '--discard-next') {
 
       if (i !== array.length - 1) {
-        array.splice(i, 1);
+        if(array[i+2] === '--double-prev' || array[i+2] === '--discard-prev') {
+          array.splice(i, 3)
+        } else {
+          array.splice(i, 2);
+        }
       } else array.splice(i, 1);
 
     } else if (array[i] === '--discard-prev') {
 
       if (i !== 0) {
         array.splice(i - 1, 1);
+        array.splice(i - 1, 1)
       } else array.splice(i, 1);
       
     } else if (array[i] === '--double-next') {
 
       if (i !== array.length - 1) {
-        array.splice(i, 0, i);
+        array.splice(i, 1);
+        array.splice(i, 0, array[i])
       } else array.splice(i, 1);
       
     } else if (array[i] === '--double-prev') {
 
       if (i !== 0) {
-        array.splice(i, 0, i - 1);
+        console.log('111')
+        array.splice(i, 1);
+        array.splice(i, 0, array[i-1]) ////////////////
       } else array.splice(i, 1);
       
     }
