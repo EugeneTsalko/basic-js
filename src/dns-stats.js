dc3358b1cf741c624dcfc4def48005d38307b940
EugeneTsalko <tefworkmail@gmail.com>
2022-05-02 17:53:02 +0300

    solve dns-stats.js

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
+function getDNSStats(domains) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result = {};
+  let reversedArr = domains.map((item) => item.split(".").reverse()); 
+  // reversedArr = [[ 'ru', 'yandex', 'code' ],[ 'ru', 'yandex', 'music' ],[ 'ru', 'yandex' ]]
+  for (let i = 0; i < reversedArr.length; i++) {
+    let dns = '';
+    for(let j = 0; j < reversedArr[i].length; j++) {
+      dns += '.' + reversedArr[i][j];
+      if (result[dns] = result[dns]) { //если такое значение ключа уже есть, то счетчик++
+        result[dns] += 1;
+      } else {
+        result[dns] = 1;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
